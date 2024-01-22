b22e1288869129e9ccbba9ca2a83db1cc241ca2e
Michał Kareńko <71224089+mkarenko@users.noreply.github.com>
2024-01-22 19:14:14 +0100

    fix: background arrow transparancy, added value of border radius

--- lib/src/controllers/factories/double_arrow_text_factory.dart
+++ lib/src/controllers/factories/double_arrow_text_factory.dart
@@ -4,11 +4,12 @@ import '../drawables/shape/double_arrrow_text_drawable.dart';
 import 'shape_factory.dart';
 
 class DoubleArrowTextFactory extends ShapeFactory<DoubleArrowTextDrawable> {
-  DoubleArrowTextFactory({this.arrowHeadSize, this.text}) : super();
+  DoubleArrowTextFactory({this.arrowHeadSize, this.text, this.roundedValue})
+      : super();
 
   double? arrowHeadSize;
   String? text;
-
+  double? roundedValue;
   @override
   DoubleArrowTextDrawable create(Offset position, [Paint? paint]) {
     return DoubleArrowTextDrawable(
@@ -16,6 +17,7 @@ class DoubleArrowTextFactory extends ShapeFactory<DoubleArrowTextDrawable> {
         position: position,
         paint: paint,
         arrowHeadSize: arrowHeadSize,
-        text: text);
+        text: text,
+        roundedValue: roundedValue);
   }
 }
